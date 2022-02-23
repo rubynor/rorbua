78b94f949ca49261a69609da25300b3ab29123e5
TrulsHafnor <Truls.Hafnor@outlook.com>
2022-02-23 15:08:04 +0100

    Validations - Endret og lagt til validations - Laget et helper metode for å displaye validation errors på en bedre måte. - Fjernet standard måte validations blir displayet

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,8 +5,9 @@ class Story < ApplicationRecord
   has_many :likes
   has_many :dislikes
 
-  validates :title, :description, presence: true
-  validates :description, length: { maximum: 250 }
+  validates :title, presence: true
+  validates :title, length: { maximum: 50 }
+  validates :description, length: { maximum: 250, too_long: "%{count} characters is the maximum allowed" }
   validates :story_file, presence: true, blob:{ content_type: :audio } #For flere validations: https://github.com/aki77/activestorage-validator
 
   def next
