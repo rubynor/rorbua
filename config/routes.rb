672ce630cfdfceab5ec852bdb7b622178a64e30c
Trygve <trygvejohannessen88@gmail.com>
2022-03-07 20:32:32 +0100

    choose language persists

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  resources :favourites
-  devise_for :users
-  resources :stories
-  resources :likes, only: [:create, :destroy]
-  resources :dislikes, only: [:create, :destroy]
-  get 'my_stories', to: 'stories#my_stories'
-  get 'play/:id', to: 'stories#play', as: 'play'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope '(:locale)' do
+    resources :favourites
+    devise_for :users
+    resources :stories
+    resources :likes, only: [:create, :destroy]
+    resources :dislikes, only: [:create, :destroy]
+    get 'my_stories', to: 'stories#my_stories'
+    get 'play/:id', to: 'stories#play', as: 'play'
 
-  # Defines the root path route ("/")
-  root "stories#index"
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    # Defines the root path route ("/")
+    root "stories#index"
+  end
 end
