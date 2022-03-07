f34b13cbd8f6ef71e4d1f98cbec87b878ef0a340
Trygve <trygvejohannessen88@gmail.com>
2022-03-07 19:58:12 +0100

    Revert "Revert "stories og favourites oversatt""
    This reverts commit e9dc633b7aeda1c1eeadfc47c49175d3e66d222e.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
 
   public def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
