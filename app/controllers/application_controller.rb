e93d5f9c92c629f28f2f859ea22c0789457327f8
Trygve <trygvejohannessen88@gmail.com>
2022-03-09 14:52:39 +0100

    kan endre username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   public def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
   def set_locale
