190c5eb583a900ce5a287a3574a4fefc2411ecc6
Alexander <alexanderlongumolsen@gmail.com>
2022-03-15 14:32:16 +0100

    Når migration kjøres nå vil alle tidligere brukere få nye spillelister "favoritter" og "mine opplastninger" som blir fylt opp med tilsvarende stories

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,29 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-  has_many :playlist_stories
+  has_many :playlist_stories, dependent: :destroy
 
   validates :title, uniqueness: { scope: :user }
+
+  def add_previous_favorites
+    favourites = @user.playlists.find_by title: "Favoritter"
+    @user.favourites.each do |story|
+      newrecord = favourites.playlist_stories.build(story: story)
+      newrecord.save
+    end
+  end
+
+  def add_previous_videos
+    videos = @user.playlists.find_by title: "Mine opplastninger"
+    @user.stories.each do |story|
+      newrecord = videos.playlist_stories.build(story: story)
+      newrecord.save
+    end
+  end
+
+  def self.add_previous(user)
+    @user = User.find(user)
+    add_previous_favorites
+    add_previous_videos
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_14_124211) do
+ActiveRecord::Schema.define(version: 2022_03_15_123813) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,12 @@ ActiveRecord::Schema.define(version: 2022_03_14_124211) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["story_id"], name: "index_favourites_on_story_id"
     t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "likes", force: :cascade do |t|
