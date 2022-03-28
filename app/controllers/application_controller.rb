ef671118de557389724c084fc48ea165fba2eada
Trygve <trygvejohannessen88@gmail.com>
2022-03-28 17:14:30 +0200

    profilbilde

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,16 @@ class ApplicationController < ActionController::Base
   public def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  end
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_image])
   end
 
   def set_locale
     I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
     session[:locale] = I18n.locale
   end
+
+  def after_sign_up_path_for(resource)
+    redirect_to profile_path(current_user)
+  end
 end
+
