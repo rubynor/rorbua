16f56d967f4587b261ceca9d05a2d1fdd28bb15d
Alexander <alexanderlongumolsen@gmail.com>
2022-03-29 10:52:39 +0200

    Fikset litt styling på audio controls, og fjernet console.log fra js controller

--- app/javascript/controllers/storydescription_controller.js
+++ app/javascript/controllers/storydescription_controller.js
@@ -4,13 +4,7 @@ export default class extends Controller {
 
     static targets = ["desc_trunc", "desc_full", "show_more", "show_less"]
 
-    connect() {
-        console.log("connected")
-    }
-
     toggleDesc(){
-        console.log("toggle")
-        console.log('Full desc: ' + this.desc_fullTarget.hidden)
         if (this.desc_truncTarget.hidden)
             this.hideDesc()
         else
@@ -18,7 +12,6 @@ export default class extends Controller {
     }
 
     showDesc(){
-        console.log("Showing desc")
         this.desc_fullTarget.hidden = false;
         this.desc_truncTarget.hidden = true;
         this.show_lessTarget.hidden = false;
@@ -27,16 +20,11 @@ export default class extends Controller {
     }
 
     hideDesc(){
-        console.log("hiding desc")
         this.desc_fullTarget.hidden = true;
         this.desc_truncTarget.hidden = false;
         this.show_lessTarget.hidden = true;
         this.show_moreTarget.hidden = false;
         window.scrollTo(0, 0);
-    }
-
-    log(){
-        console.log("click");
     }
 
 }
