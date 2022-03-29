5d71b7cd9f0ae437a409cb86577605e502dde88f
Trygve <trygvejohannessen88@gmail.com>
2022-03-29 12:01:28 +0200

    se bruker sine stories på profil side

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   # GET /profiles/1 or /profiles/1.json
   def show
     @user= User.find(params[:id])
+    @playlist = @user.playlists.find_by("title='Mine opplastninger'")
   end
 
 end
