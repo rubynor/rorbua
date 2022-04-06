23b3ccfaadfb3d662cd4afef5b98b5e188067a2b
TrulsHafnor <Truls.Hafnor@outlook.com>
2022-04-06 15:13:47 +0200

    Det funker å å lage reports til Stories

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,8 @@
 class Report < ApplicationRecord
   belongs_to :reportable, polymorphic: true
 
+  validates :description, :status, :reportable_type, :reportable_id, presence: true
+  validates :description, length: { maximum: 250 }
 
+  enum status: { Aktiv: 0, Avsluttet: 1 }
 end
