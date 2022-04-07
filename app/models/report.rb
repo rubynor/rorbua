3aff60eca0da24405c1c1468d7deb5a64d18993e
TrulsHafnor <Truls.Hafnor@outlook.com>
2022-04-07 15:09:10 +0200

    basic commit

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,5 @@
 class Report < ApplicationRecord
   belongs_to :reportable, polymorphic: true
-
   validates :description, :status, :reportable_type, :reportable_id, presence: true
   validates :description, length: { maximum: 250 }
 
