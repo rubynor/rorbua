6552e7c1039d1b29e96f4a3d6f18c442a9758ca8
Trygve <trygvejohannessen88@gmail.com>
2022-04-19 14:45:37 +0200

    slettet credentials og endret tilbake den andre fila

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_action only: [:destroy] do
-    delete_from_aws("#{current_user.user_image.key}")
+  before_action only: [:destroy, :update] do
+    if @user.user_image.attached?
+      delete_from_aws("#{current_user.user_image.key}")
+    end
   end
-
-  def destroy
-    resource.destroy
-    set_flash_message :notice, :destroyed
-    sign_out_and_redirect(self.resource)
-  end
-
 end
