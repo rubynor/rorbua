621d0589c5883c3157d1f87530a7579e1f1a163f
TrulsHafnor <Truls.Hafnor@outlook.com>
2022-04-25 14:00:12 +0200

    Design og lagt til Tittel

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,9 @@ class Report < ApplicationRecord
   belongs_to :reportable, polymorphic: true
 
   enum status: { Aktiv: 0, Innaktiv: 1 }
+  enum tittle: { "Uppassende ting": 0,
+                 "Veldig ting": 1,
+                 "sykt ting":2}
   validates :description, length: { maximum: 500 }
-  validates :description, :status, :userId, :reportable_id, :reportable_type, presence: true
+  validates :description, :status, :userId, :reportable_id, :reportable_type, :tittle, presence: true
 end
